ffd5b146e2ba06598dfebc915c540748850092fd
Tami Williams <tami@asktami.com>
2019-08-13 15:42:52 -0400

    added lifecycle notes

--- src/state-drills/BombAlt.js
+++ src/state-drills/BombAlt.js
@@ -8,6 +8,17 @@ export default class Bomb extends Component {
 		count: 0
 	};
 
+	/*
+	componentDidMount() is called as soon as the component is mounted and ready
+
+	- this is a good place to initiate API calls, if you need to load data from a remote endpoint
+
+	- componentDidMount() allows the use of setState()
+
+	- calling the setState() here will update state and cause another rendering but it will happen before the browser updates the UI. This is to ensure that the user will not see any UI updates with the double rendering.
+
+	- You can set state here but with caution.
+	*/
 	componentDidMount() {
 		this.interval = setInterval(() => {
 			this.setState({
@@ -16,6 +27,15 @@ export default class Bomb extends Component {
 		}, 1000);
 	}
 
+	/*
+	componentWillUnmount()
+
+	As the name suggests this lifecycle method is called just before the component is unmounted and destroyed. If there are any cleanup actions that you would need to do, this would be the right spot.
+
+	You cannot modify the component state in componentWillUnmount lifecycle.
+
+	Common cleanup activities performed in this method include, clearing timers, cancelling api calls, or clearing any caches in storage.
+	*/
 	componentWillUnmount() {
 		clearInterval(this.interval);
 	}
@@ -32,6 +52,9 @@ export default class Bomb extends Component {
 		}
 	}
 
+	// render() = mount component
+	// you can not setState() within a render()
+	// because a render() method has to be pure with no side-effects (will always return the same output when the same inputs are passed)
 	render() {
 		return <div className="CountdownBomb">{this.renderDisplay()}</div>;
 	}
